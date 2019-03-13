012539a3075b3880dd26d309296fdd648a42e039
Barry <barryboayes17@gmail.com>
2019-03-13 03:39:28 -0700

    implemented service worker to work offline

---
+++ sw.js
@@ -0,0 +1,30 @@
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open('restaurant-cache').then(function(cache){
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/restaurant.html',
+                // 'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'css/responsive_details.css',
+                'css/responsive.css',
+                'css/styles.css',
+                'img/',
+                'data/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(res){
+            if(res) return res;
+            return fetch(event.request);
+        })
+    );
+});
